920ee51cd16a338c7b75572ea100b2c2414584a8
jy <78412709@qq.com>
2022-11-20 23:18:10 +0800

    员工功能开发

--- src/main/java/com/itheima/ReggieTakeOutApplication.java
+++ src/main/java/com/itheima/ReggieTakeOutApplication.java
@@ -2,8 +2,10 @@ package com.itheima;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan   // 开启组件扫描，扫描添加的请求过滤器
 public class ReggieTakeOutApplication {
 
     public static void main(String[] args) {
