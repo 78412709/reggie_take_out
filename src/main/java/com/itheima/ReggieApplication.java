a544b26c3c3050531acdff9738bec753fd5e4bb0
jy <78412709@qq.com>
2022-11-21 23:30:17 +0800

    菜品管理功能开发

--- src/main/java/com/itheima/ReggieApplication.java
+++ src/main/java/com/itheima/ReggieApplication.java
@@ -1,15 +1,19 @@
 package com.itheima;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@Slf4j
 @SpringBootApplication
 @ServletComponentScan   // 开启组件扫描，扫描添加的请求过滤器
-public class ReggieTakeOutApplication {
+@EnableTransactionManagement
+public class ReggieApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ReggieTakeOutApplication.class, args);
+        SpringApplication.run(ReggieApplication.class, args);
+        log.info("项目启动成功。。。");
     }
-
 }
